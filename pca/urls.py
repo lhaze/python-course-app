19af8f1455cc25ab4e726f50f75d26765c9457d7
Lukasz Haze <lhaze@lhaze.name>
2018-03-06 02:16:44 +0100

    Index page

--- pca/urls.py
+++ pca/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from pca.utils.config import env_var
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('users/', include('pca.users.urls')),
     path('admin-{}/'.format(env_var('DJANGO_ADMIN_PATH', 'foo')), admin.site.urls),
 ]
